3c8a914e333f3757e81ed77a9c294fc711b6c257
Vusala05 <167974345+Vusala05@users.noreply.github.com>
2024-08-11 18:05:46 +0400

    data is received in Result_fragment.kt

--- app/src/main/java/com/example/calculatoractivity/Result_fragment.kt
+++ app/src/main/java/com/example/calculatoractivity/Result_fragment.kt
@@ -5,8 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.calculatoractivity.databinding.FragmentResultFragmentBinding
 
 
 class Result_fragment : Fragment(R.layout.fragment_result_fragment) {
+    private lateinit var binding: FragmentResultFragmentBinding
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentResultFragmentBinding.bind(view)
+
+        arguments?.let {
+            val result = Result_fragmentArgs.fromBundle(it).result
+            binding.result.text = result
+        }
+        binding.back.setOnClickListener {
+            val action = Result_fragmentDirections.actionResultFragmentToCalculateFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
 }
+
