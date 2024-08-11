78e5e2dea28c6956aa49d2518991f633ad314e00
Vusala05 <167974345+Vusala05@users.noreply.github.com>
2024-08-11 17:34:25 +0400

    function for sum operation is created

--- app/src/main/java/com/example/calculatoractivity/Calculate_fragment.kt
+++ app/src/main/java/com/example/calculatoractivity/Calculate_fragment.kt
@@ -5,8 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.calculatoractivity.databinding.FragmentCalculateFragmentBinding
 
 
 class Calculate_fragment : Fragment(R.layout.fragment_calculate_fragment) {
+
+        private lateinit var binding: FragmentCalculateFragmentBinding
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            binding = FragmentCalculateFragmentBinding.bind(view)
+
+            setupPlus()
+
+        }
+
+        private fun setupPlus() {
+            binding.plus.setOnClickListener {
+                val firstNum = binding.firstNum.text.toString().toDoubleOrNull() ?: 0.0
+                val secondNum = binding.secondNum.text.toString().toDoubleOrNull() ?: 0.0
+                val result = firstNum + secondNum
+                val action = Calculate_fragmentDirections.actionCalculateFragmentToResultFragment(result.toString())
+                Navigation.findNavController(it).navigate(action)
+            }
+        }
+
 
 }
